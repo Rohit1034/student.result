0c81d28fad11be42d66d350097bfa342ba35fae5
Rohit1034 <138391793+Rohit1034@users.noreply.github.com>
2023-07-26 01:21:52 +0530

    Add files via upload

---
+++ resultresult.py
@@ -0,0 +1,57 @@
+rahul = int(input("Rahul : "))
+aadesh = int(input("Aadesh : "))
+rohit = int(input("Rohit : "))
+sakshee = int(input("Sakshee : "))
+if rohit != int:
+        print("invalid entry")
+if rahul>90:
+       l = print("Rahul result : A+")
+elif rahul>80:
+       l = print("Rahul result : A")
+elif rahul>70:
+       l = print("Rahul result : B")
+elif rahul>60:
+       l = print("Rahul result : c")
+elif rahul>50:
+      l= print("Rahul result : D")
+else:
+       l = print("Rahul result : fail")
+if aadesh>90:
+        a = print("Aadesh resut : A+")
+elif aadesh>80:
+    
+        a= print("Aadesh resut : A")
+elif aadesh>70:
+        a= print("Aadesh resut : B")
+elif aadesh>60:
+        a= print("Aadesh resut : C")
+elif aadesh>50:
+        a = print("Aadesh resut : D")
+else:
+        a = print("Aadesh resut : fail")
+    
+if rohit>90:
+        r = print("Rohit result : A+")
+elif rohit>80:
+        r = print("Rohit result : A")
+elif rohit>70:
+        r = print("Rohit result : B")
+elif rohit>60:
+        r = print("Rohit result : C")
+elif rohit>50:
+        r = print("Rohit result : D")
+else:
+        r = print("Rohit result : fail")
+
+if sakshee>90:
+        s = print("Sakshee result : A+")
+elif sakshee>80:
+        s  = print("Sakshee result : A")
+elif sakshee>70:
+        s = print("Sakshee result : B")
+elif sakshee>60:
+        s = print("Sakshee result : C")
+elif sakshee>50:
+        s = print("Sakshee result : D")
+else:
+        s = print("Sakshee result : fial")  
